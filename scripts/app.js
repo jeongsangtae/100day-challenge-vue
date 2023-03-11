0e291569261b1af09b20a611af69a9e3bdae336c
jeongsangtae <wjdtkdxo55@naver.com>
2023-03-12 00:04:38 +0900

    Vue - Listen to events & update data

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,25 @@
-// const TodosApp = {
-//   data() {
-//     return {
-//       newTodo: "Vue.js 배우기!",
-//     };
-//   },
-// };
-
-// Vue.createApp(TodosApp).mount("#todos-app");
-
-const TodosApp = Vue.createApp({
+const TodosApp = {
   data() {
     return {
       newTodo: "Vue.js 배우기!",
     };
   },
-});
+  methods: {
+    saveTodo(event) {
+      event.preventDefault();
+      this.newTodo = "업데이트 To do";
+    },
+  },
+};
 
-TodosApp.mount("#todos-app");
+Vue.createApp(TodosApp).mount("#todos-app");
+
+// const TodosApp = Vue.createApp({
+//   data() {
+//     return {
+//       newTodo: "Vue.js 배우기!",
+//     };
+//   },
+// });
+
+// TodosApp.mount("#todos-app");
